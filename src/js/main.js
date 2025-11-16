2d452d4285de83abfbc9fab6eee29fe417054f5e
ernestsb1 <ernestnyore2020@gmail.com>
2025-11-16 21:39:45 +0100

    Added

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,31 @@
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
+
+
+// Toggle the entire FAQ section
+const faqToggle = document.querySelector('.faq-toggle');
+const faqSection = document.querySelector('.faq-section');
+
+faqToggle.addEventListener('click', () => {
+  if (faqSection.style.maxHeight) {
+    faqSection.style.maxHeight = null;
+  } else {
+    faqSection.style.maxHeight = faqSection.scrollHeight + 'px';
+  }
+});
+
+// Individual FAQ items collapsible
+const faqQuestions = document.querySelectorAll('.faq-question');
+
+faqQuestions.forEach((question) => {
+  question.addEventListener('click', () => {
+    const answer = question.nextElementSibling;
+
+    if (answer.style.maxHeight) {
+      answer.style.maxHeight = null;
+    } else {
+      answer.style.maxHeight = answer.scrollHeight + 'px';
+    }
+  });
+});
