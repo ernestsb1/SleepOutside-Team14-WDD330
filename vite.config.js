bd97e28b9b255c5588a527dc3ba342b2be262ae8
ernestsb1 <ernestnyore2020@gmail.com>
2025-12-02 22:41:08 +0100

    Updated Vite

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,10 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
-        product_listing: resolve(__dirname, "src/product_listing/index.html"),
+        main: resolve(__dirname, "src/index.html"),         // home page
+        book: resolve(__dirname, "src/pages/book.html"),    // book details
+        lists: resolve(__dirname, "src/pages/lists.html"),  // reading list selection
+        list: resolve(__dirname, "src/pages/list.html"),    // individual reading list
       },
     },
   },
